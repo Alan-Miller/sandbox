b9644d85645bfe43cbeb37834a42e064557d8b8a
Alan-Miller <alan.ashman@gmail.com>
2017-06-26 22:14:53 -0600

    minor edits

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from 'registerServiceWorker';
+
+
+
+ReactDom.render(<App id="App"/>, document.getElementById('root'));
+registerServiceWorker();
